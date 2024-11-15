3979047e80e130c6fd662d2ada6b967919a285c9
MarriwadaSowmya <Sowmyamarriwada@gmail.com>
2024-11-15 10:25:40 +0530

    Create Vignesh_and_GYM.java

---
+++ vissa_prep/Vignesh_and_GYM.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        
+        if (x <= z && x+y > z){
+            System.out.println("1");
+        }
+        else if (x+y <= z){
+            System.out.println("2");
+        } 
+        else {
+            System.out.println("0");
+        }
+    }
+}
